f1bcb5367aaba9cbb0c15a00217ba33cd650eef4
Diksha Verma <162664397+dfirev@users.noreply.github.com>
2025-06-21 17:27:03 +0530

    script.js

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
-let num=Math.floor(Math.random()*10)+1;
-let guess=null;
-while(num!==guess){
-    guess=Number(prompt("Enter number"));
-    if(num>guess){
-        alert("too low");
+let birthYear = document.getElementById("birthYear");
+let ageBtn = document.getElementById("ageBtn");
+let ageResult = document.getElementById("ageResult");
+let clear = document.getElementById("clear");
+ageBtn.addEventListener("click",age);
+clear.addEventListener("click",clean);
+function age(){
+    let birthYearValue = birthYear.value;
+    let currentYear = new Date().getFullYear(); 
+    let ageRightNow = currentYear-Number(birthYearValue);
+    if( birthYearValue === "" ){
+        ageResult.innerText = "Please enter an year..";
     }
-    else if(num<guess){
-        alert("too high");
+    else if(isNaN(birthYearValue)){
+        ageResult.innerText = "Please enter an year";
+    }
+    else if(ageRightNow < 0){
+        ageResult.innerText = "You are inputing wrong year";
+    }
+    else if(ageRightNow > 125){
+        ageResult.innerText=" Hey u oldiee...\n The oldest person alive is 123 years old.. and u are older then them??"
+    }
+    else{
+        ageResult.innerText = " You are " + ageRightNow + " years old ! ";
     }
 }
-alert("congrats")
+function clean(){
+    ageResult.innerText="";
+    birthYear.value="";
+}
